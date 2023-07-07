085eaa679260883b23dee8cbf8f5e130df566887
isaac jaime <isaacjaimep@gmail.com>
2023-07-06 20:35:18 -0600

    ok

---
+++ src/main/java/com/example/mydevops2/Mydevops2Application.java
@@ -0,0 +1,13 @@
+package com.example.mydevops2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mydevops2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mydevops2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mydevops2/Mydevops2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydevops2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mydevops2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
